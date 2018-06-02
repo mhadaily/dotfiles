772ea583c72797ad59f57c850c885923738f303e
Majid Hajian <mhadaily@gmail.com>
2018-06-02 12:48:24 +0200

    hide hardrive on desktop

--- os/preferences/macos/finder.sh
+++ os/preferences/macos/finder.sh
@@ -34,8 +34,8 @@ execute "defaults write com.apple.finder NewWindowTarget -string 'PfDe' && \
          defaults write com.apple.finder NewWindowTargetPath -string 'file://$HOME/'" \
     "Set '~' as the default location for new Finder windows"
 
-#defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true && \ hard drives,
-execute "defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true && \
+#defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true && \ hard drives,
+execute "defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true && \
          defaults write com.apple.finder ShowMountedServersOnDesktop -bool true && \
          defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true" \
     "Show icons for  servers, and removable media on the desktop"
